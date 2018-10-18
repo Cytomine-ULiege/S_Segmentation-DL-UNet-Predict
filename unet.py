d743894884b71373fe514aca1660453835c02a61
Waliens <romainmormont@hotmail.com>
2018-10-18 20:01:47 +0200

    update architecture

--- unet.py
+++ unet.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 from cytomine.models import AttachedFileCollection, Property
 from keras import Input, Model
-from keras.layers import Convolution2D, LeakyReLU, SpatialDropout2D, AveragePooling2D, UpSampling2D, merge
+
+from keras.layers import *
 
 
 def load_model(job, download_path, model_filename="weights.hf5"):
@@ -34,50 +35,47 @@ def load_data(cj, dims, path, **monitor_params):
 
 
 def create_unet(dims):
-    input = Input(shape=(dims[0], dims[1], dims[2]))
+    inputs = Input(dims)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    conv1 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(input)
-    conv1 = LeakyReLU()(conv1)
-    conv1 = SpatialDropout2D(0.2)(conv1)
-    conv1 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(conv1)
-    conv1 = LeakyReLU()(conv1)
-    conv1 = SpatialDropout2D(0.2)(conv1)
-    pool1 = AveragePooling2D(pool_size=(2, 2))(conv1)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
 
-    conv2 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(pool1)
-    conv2 = LeakyReLU()(conv2)
-    conv2 = SpatialDropout2D(0.2)(conv2)
-    conv2 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(conv2)
-    conv2 = LeakyReLU()(conv2)
-    conv2 = SpatialDropout2D(0.2)(conv2)
-    pool2 = AveragePooling2D(pool_size=(2, 2))(conv2)
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4, up6])
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
-    conv3 = Convolution2D(128, 3, 3, border_mode='same', init='he_normal')(pool2)
-    conv3 = LeakyReLU()(conv3)
-    conv3 = SpatialDropout2D(0.2)(conv3)
-    conv3 = Convolution2D(128, 3, 3, border_mode='same', init='he_normal')(conv3)
-    conv3 = LeakyReLU()(conv3)
-    conv3 = SpatialDropout2D(0.2)(conv3)
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3, up7])
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
-    comb1 = merge([conv2, UpSampling2D(size=(2, 2))(conv3)], mode='concat', concat_axis=1)
-    conv4 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(comb1)
-    conv4 = LeakyReLU()(conv4)
-    conv4 = SpatialDropout2D(0.2)(conv4)
-    conv4 = Convolution2D(64, 3, 3, border_mode='same', init='he_normal')(conv4)
-    conv4 = LeakyReLU()(conv4)
-    conv4 = SpatialDropout2D(0.2)(conv4)
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2, up8])
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
-    comb2 = merge([conv1, UpSampling2D(size=(2, 2))(conv4)], mode='concat', concat_axis=1)
-    conv5 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(comb2)
-    conv5 = LeakyReLU()(conv5)
-    conv5 = SpatialDropout2D(0.2)(conv5)
-    conv5 = Convolution2D(32, 3, 3, border_mode='same', init='he_normal')(conv5)
-    conv5 = LeakyReLU()(conv5)
-    conv5 = SpatialDropout2D(0.2)(conv5)
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1, up9])
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
-    output = Convolution2D(1, 1, 1, activation='sigmoid')(conv5)
-
-    model = Model(input=input, output=output)
+    model = Model(inputs=[inputs], outputs=[conv10])
     return model
-
 
